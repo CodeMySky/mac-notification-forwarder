38a7d81a54841b3675052e844ad3c47755b671bb
Sean Han <hanxiaobow@gmail.com>
2024-04-29 19:57:20 -0700

    Ability to create new channel (#11)
    * Ability to create new channel
    
    * nit

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ poetry install
 ```
 - [Install slack app](https://slack.com/oauth/v2/authorize?client_id=155947593521.6629501796849&scope=app_mentions:read,channels:join,channels:read,chat:write,chat:write.public,groups:write,im:write,mpim:write,reminders:write&user_scope=)
 - Get slack api key and save it to `SLACK_API_KEY` variable, to get slack oauth token, go to https://api.slack.com/apps/A06JHERPEQZ/oauth
+- If channel setting is set to a channel id, all messages will be send to that channel, if default, it will create channels based on app id
 
 
 # To Run
--- mnf/notification_forwarder.py
+++ mnf/notification_forwarder.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 import os
+import logging
 from slack_sdk import WebClient
 
 from mnf.notification import Notification
@@ -7,6 +8,8 @@ from mnf.notification import Notification
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 
 class BaseNotificationForwarder:
@@ -20,15 +23,40 @@ class BaseNotificationForwarder:
 
 class SlackNotificationForwarder(BaseNotificationForwarder):
 
-    def __init__(self, channel: str) -> None:
+    def __init__(self, channel_setting: str) -> None:
         super().__init__()
         slack_api_key = os.environ.get("SLACK_API_KEY")
-        assert slack_api_key is not None, "SLACK_API_KEY is not set"
+        if slack_api_key is None:
+            logger.error("SLACK_API_KEY is not set")
         self.client = WebClient(slack_api_key)
-        self.channel = channel
+        self.channel_setting = channel_setting
+        self.channel_to_id_map = {}
+        channels = self.client.conversations_list()
+        for channel in channels["channels"]:
+            self.channel_to_id_map[channel["name"]] = channel["id"]
+
+    def create_or_get_channel_id(self, name: str) -> str:
+        if self.channel_setting != "default":
+            return self.channel_setting
+        name = name.lower().split(".")[-1]
+        if name not in self.channel_to_id_map:
+            logger.info(f"Creating new channel {name}")
+            try:
+                res = self.client.conversations_create(name=name)
+                self.channel_to_id_map[name] = res["channel"]["id"]
+                slack_user = os.environ.get("SLACK_USER_ID")
+                if slack_user is not None and slack_user != "":
+                    self.client.conversations_invite(
+                        channel=res["channel"]["id"], users=slack_user
+                    )
+            except Exception as e:
+                logger.error(e)
+
+        return self.channel_to_id_map[name]
 
     def send_notification(self, notification: Notification) -> None:
+        channel_id = self.create_or_get_channel_id(notification.app_id)
         self.client.chat_postMessage(
-            channel=self.channel, text=f"{notification.title}\n{notification.body}"
+            channel=channel_id, text=f"{notification.title}\n{notification.body}"
         )
         return
